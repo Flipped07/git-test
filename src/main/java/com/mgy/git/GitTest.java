cde3d84ab51a405a24f7a133ac65dc736fa935cc
yang <728818834@qq.com>
2023-01-30 17:41:25 +0800

    first commit

---
+++ src/main/java/com/mgy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.mgy.git;
+
+/**
+ * @author 蒙桂阳
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
